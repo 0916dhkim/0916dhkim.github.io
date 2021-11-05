59fb93b45695c2444cafd0176185b24b3b5244a9
Donghyeon Kim <0916dhkim@gmail.com>
2021-11-04 21:57:20 -0400

    Move Articles to Data Folder

--- lib/projectPaths.ts
+++ lib/projectPaths.ts
@@ -1,4 +1,4 @@
 import path from "path";
 export const ROOT = process.cwd();
-export const ARTICLES_FOLDER = path.join(ROOT, "components", "articles");
+export const ARTICLES_FOLDER = path.join(ROOT, "data", "articles");
 export const PUBLIC_FOLDER = path.join(ROOT, "public");
